b36c9ee2286a879f241faa1c003bc34dcb8b88e2
jbreezyasf <juanita.brazziel@gmail.com>
2026-08-21 07:31:28 -0500

    Load richer Gate 5 stadium styling

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,7 @@ import './dashboard.css';
 import './gate5.css';
 import './mobile-nav.css';
 import './forms-gate5.css';
+import './stadium-gate5.css';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://bigexecfs.com'),
