2362d0dc2c626c13d595d142ce01e281dde936fc
jbreezyasf <juanita.brazziel@gmail.com>
2026-08-22 22:23:46 -0500

    Unify league pages under shared app header

--- apps/web/app/leagues/[leagueId]/layout.tsx
+++ apps/web/app/leagues/[leagueId]/layout.tsx
@@ -1,15 +1,10 @@
 import BigExecMobileNav from '../../components/BigExecMobileNav';
+import BigExecAppHeader from '../../components/BigExecAppHeader';
 
 export default async function LeagueLayout({children,params}:{children:React.ReactNode;params:Promise<{leagueId:string}>}) {
   const {leagueId}=await params;
   return <>
-    <nav className="leagueNav" aria-label="League navigation">
-      <a href={`/leagues/${leagueId}`}>HQ</a>
-      <a href={`/leagues/${leagueId}/locker-room`}>Locker Room</a>
-      <a href={`/leagues/${leagueId}/schedule`}>Schedule</a>
-      <a href={`/leagues/${leagueId}/trades`}>Trades</a>
-      <a href={`/leagues/${leagueId}/players`}>Players</a>
-    </nav>
+    <BigExecAppHeader leagueId={leagueId}/>
     {children}
     <BigExecMobileNav leagueId={leagueId}/>
   </>;
